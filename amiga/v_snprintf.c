f5646d2b2e3ec258f4c97db7aefd44cc7d614686
hvr <hvr@gnu.org>
2001-08-03 19:40:06 +0000

    minor fix

--- amiga/v_snprintf.c
+++ amiga/v_snprintf.c
@@ -2,18 +2,17 @@
 #include <stdarg.h>
 #include <string.h>
 
-
 int
 vsnprintf (char *str, size_t n, const char *fmt, va_list ap)
 {
   int ret;
-  char buffer[4096];
+  char buffer[4096] = { 0, };
 
   if ((int) n < 1)
     return (EOF);
 
-  memset (buffer, 0, 4096);
   ret = vsprintf (buffer, fmt, ap);
+
   if (ret >= n)
     {
       memcpy (str, buffer, n);
@@ -25,17 +24,23 @@ vsnprintf (char *str, size_t n, const char *fmt, va_list ap)
       str[ret] = '\0';
     }
 
+  if (ret>=n)
+    ret = -1;
+
   return (ret);
 }
-
 
 int
 snprintf (char *s, size_t size, const char *format, ...)
 {
   int retval;
   va_list args;
+
   va_start (args, format);
   retval = vsnprintf (s, size, format, args);
   va_end (args);
+
   return retval;
 }
+
+/* EOF */
